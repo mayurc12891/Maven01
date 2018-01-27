09a4cec443f6558db44be3a6801f2d4b7bbad727
kit <mayur12891@gmail.com>
2018-01-27 16:14:05 +0530

    commitone

---
+++ src/test/java/Maven01/Mavenfirst/Class01.java
@@ -0,0 +1,14 @@
+package Maven01.Mavenfirst;
+
+import org.testng.annotations.Test;
+
+public class Class01 {
+	
+	@Test
+	public void Testcase01() {
+		
+		System.out.println("This is Testcase001");
+		
+	}
+
+}
