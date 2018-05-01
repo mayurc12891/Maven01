70a050473153bea5aa4636be2c22ed569f1745aa
kit <mayur12891@gmail.com>
2018-05-01 18:20:07 +0530

    Added assert in all TCs

--- src/test/java/Maven01/Mavenfirst/Class01.java
+++ src/test/java/Maven01/Mavenfirst/Class01.java
@@ -1,5 +1,6 @@
 package Maven01.Mavenfirst;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Class01 {
@@ -7,33 +8,33 @@ public class Class01 {
 //  new comment added	
 	@Test
 	public void Testcase01() {
-		
+		Assert.assertTrue(false);
 		System.out.println("This is Testcase001");
 		
 	}
 	
 	@Test
 	public void Testcase02() {
-		
+		Assert.assertTrue(false);
 		System.out.println("This is testcase002");
 		
 	}
 	
 	@Test
 	public void Testcase03() {
-		
+		Assert.assertTrue(false);
 		System.out.println("This is testcase003");
 		
 	}
 	@Test
 	public void Testcase04() {
-		
+		Assert.assertTrue(false);
 		System.out.println("This is testcase004");
 		
 	}
 	@Test
 	public void Testcase05() {
-		
+		Assert.assertTrue(false);
 		System.out.println("This is testcase005");
 		
 	}
